2b5289b86284be9ba1dfea88a019918b697f6f9d
jacobproffer <jacobhardingproffer@gmail.com>
2026-01-07 20:54:48 -0500

    fix: update site URL and enhance RuneMetrics data fetching with error handling and validation

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,7 +6,7 @@ import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://www.example.com",
+  site: "https://astro-runescape.netlify.app",
   output: "server",
   adapter: netlify(),
   integrations: [sitemap()],
